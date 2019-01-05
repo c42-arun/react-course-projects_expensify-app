7d982685c7652f37632065dafed09bf1c9bc220e
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2019-01-05 21:53:02 +0000

    Organising routes: Step 2 - components broken into their own files

---
+++ src/components/EditExpensePage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const EditExpensePage = () => (
+    <div>This is the expensify edit component</div>
+);
+
+export default EditExpensePage;
--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,39 +1,13 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-const ExpenseDashboardPage = () => (
-    <div>This is the expensify dashboard component</div>
-);
+import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
+import AddExpensePage from '../components/AddExpensePage';
+import EditExpensePage from '../components/EditExpensePage';
+import HelpPage from '../components/HelpPage';
+import NotFoundPage from '../components/NotFoundPage';
+import Header from '../components/Header';
 
-const ExpenseCreatePage = () => (
-    <div>This is the expensify create component</div>
-);
-
-const ExpenseEditPage = () => (
-    <div>This is the expensify edit component</div>
-);
-
-const HelpPage = () => (
-    <div>This is the expensify help component</div>
-);
-
-const NotFoundPage = () => (
-    <div>
-        404! <Link to="/">Go home</Link>
-    </div>
-);
-
-const Header = () => (
-    <header>
-        <h1>Expensify</h1>
-        <ul>
-            <li><NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink></li>
-            <li><NavLink to="/create" activeClassName="is-active">Create Expense</NavLink></li>
-            <li><NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink></li>
-            <li><NavLink to="/help" activeClassName="is-active">Help</NavLink></li>
-        </ul>
-    </header>
-);
 
 const AppRouter = () => (
     <BrowserRouter>
@@ -41,8 +15,8 @@ const AppRouter = () => (
             <Header />
             <Switch>
                 <Route path="/" component={ExpenseDashboardPage} exact={true} />
-                <Route path="/create" component={ExpenseCreatePage} />
-                <Route path="/edit" component={ExpenseEditPage} />
+                <Route path="/create" component={AddExpensePage} />
+                <Route path="/edit" component={EditExpensePage} />
                 <Route path="/help" component={HelpPage} />
                 <Route component={NotFoundPage} />
             </Switch>
