6e20ffd2f8bf17cdf0ee48da75fe2dafdc191dde
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2019-01-05 21:25:50 +0000

    header layout; NavLink - to style navigation links

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
 
@@ -26,16 +26,30 @@ const NotFoundPage = () => (
     </div>
 );
 
+const Header = () => (
+    <header>
+        <h1>Expensify</h1>
+        <ul>
+            <li><NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink></li>
+            <li><NavLink to="/create" activeClassName="is-active">Create Expense</NavLink></li>
+            <li><NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink></li>
+            <li><NavLink to="/help" activeClassName="is-active">Help</NavLink></li>
+        </ul>
+    </header>
+);
 
 const routes = (
     <BrowserRouter>
-        <Switch>
-            <Route path="/" component={ExpenseDashboardPage} exact={true} />
-            <Route path="/create" component={ExpenseCreatePage} />
-            <Route path="/edit" component={ExpenseEditPage} />
-            <Route path="/help" component={HelpPage} />
-            <Route component={NotFoundPage} />
-        </Switch>
+        <div>
+            <Header />
+            <Switch>
+                <Route path="/" component={ExpenseDashboardPage} exact={true} />
+                <Route path="/create" component={ExpenseCreatePage} />
+                <Route path="/edit" component={ExpenseEditPage} />
+                <Route path="/help" component={HelpPage} />
+                <Route component={NotFoundPage} />
+            </Switch>
+        </div>
     </BrowserRouter>
 );
 
