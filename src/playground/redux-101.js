451ed0723880409a0fa5f80bf22d9a6775942409
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2019-01-07 07:38:51 +0000

    First basic Action; fixed variable naming

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,7 +1,30 @@
 import { createStore } from 'redux';
 
-const state = createStore((state = { count: 0 }) => {
-    return state;
+const store = createStore((state = { count: 0 }, action) => {
+    if (action.type === 'INCREMENT') {
+        return {
+            count: state.count + 1
+        }
+    } else {
+        return state;
+    }
 });
 
-console.log(state.getState());
+console.log(store.getState()); // prints {count: 0}
+
+// Action is just an object that gets sent to the store (via store.dispatch())
+// so the store can decide if & how to modify the state based on the information contained
+// in that object
+
+const action = {
+    type: 'INCREMENT'
+};
+
+store.dispatch(action);
+
+console.log(store.getState()); // prints {count: 1}
+
+store.dispatch(action);
+store.dispatch(action);
+
+console.log(store.getState()); // prints {count: 3}
