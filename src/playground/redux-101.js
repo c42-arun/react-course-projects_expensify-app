2a3204a9c740321591a8ef092bbef2441286033e
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2019-01-07 11:33:09 +0000

    Actions with payload; action subscriptions

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,30 +1,82 @@
 import { createStore } from 'redux';
 
 const store = createStore((state = { count: 0 }, action) => {
-    if (action.type === 'INCREMENT') {
-        return {
-            count: state.count + 1
-        }
-    } else {
-        return state;
+    switch (action.type) {
+        case 'INCREMENT':
+            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
+            return {
+                count: state.count + incrementBy
+            }
+
+        case 'DECREMENT':
+            return {
+                count: state.count - 1
+            }
+
+        case 'RESET':
+            return {
+                count: 0
+            }
+
+        case 'SET':
+            return {
+                count: action.setValue
+            }
+
+        default:
+            return state;
     }
 });
 
-console.log(store.getState()); // prints {count: 0}
+// subscribe to any action calls into reducer 
+// also returns a function to unsubscribe at any time
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState());
+});
 
-// Action is just an object that gets sent to the store (via store.dispatch())
-// so the store can decide if & how to modify the state based on the information contained
-// in that object
-
-const action = {
+// prints {count: 1}
+store.dispatch({
     type: 'INCREMENT'
-};
+});
 
-store.dispatch(action);
+// NOTE: another member to pass additional data - prints {count: 6}
+store.dispatch({
+    type: 'INCREMENT',
+    incrementBy: 5
+});
 
-console.log(store.getState()); // prints {count: 1}
+// prints {count: 5}
+store.dispatch({
+    type: 'DECREMENT'
+});
 
-store.dispatch(action);
-store.dispatch(action);
+// prints {count: 0}
+store.dispatch({
+    type: 'RESET'
+});
 
-console.log(store.getState()); // prints {count: 3}
+// prints {count: -10}
+store.dispatch({
+    type: 'SET',
+    setValue: -10
+});
+
+// subscription is called even if the action isn't defined
+store.dispatch({
+    type: 'DOES_NOT_EXIST'
+});
+
+// prints {count: undefined}
+store.dispatch({
+    type: 'SET',
+    // setValue: -10
+});
+
+// unsubsribe from store changes (i.e whenever reducer function is called)
+unsubscribe();
+
+// unsubscribe not called so no console.log()
+store.dispatch({
+    type: 'SET',
+    setValue: 100
+});
