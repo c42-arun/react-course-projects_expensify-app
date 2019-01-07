5ab6a78ebf289328ff9d5f4eb272bfb7f067b66b
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2019-01-07 14:49:23 +0000

    Add Expense action - note how it is handled only by expensesReducer

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,14 +1,46 @@
 import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid';
+
+// ** Expense Actions **
+// ADD_EXPENSE
+const addExpense = ( { description='', note='', amount=0 } = {}) => ({
+    type: 'ADD_EXPENSE',
+    expense: {
+        id: uuid(),
+        description,
+        note,
+        amount
+    }
+});
+
+// REMOVE_EXPENSE
+// EDIT_EXPENSE
 
 // Expenses Reducer
 const expensesReducerDefaultState = [];
 
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
     switch (action.type) {
+        case 'ADD_EXPENSE':
+            // this is equivalent to 'return state.concat(action.expense)
+            // both return a new array without modifying original 'state' array
+            return [
+                ...state,
+                action.expense
+            ];
+
         default:
             return state;
     }
 };
+
+
+// ** Filter Actions **
+// SET_FILTER_TEXT
+// SET_SORT_BY_AMOUNT
+// SET_SORT_BY_DATE
+// FILTER_BY_START_DATE
+// FILTER_BY_END_DATE
 
 // Filters Reducer
 const filtersReducerDefaultState = {
@@ -34,7 +66,14 @@ const store = createStore(
     })
 );
 
-console.log(store.getState()); // now would print state as { expenses: [], filters: { <filtersReducerDefaultState>} }
+store.subscribe(() => {
+    console.log(store.getState()); 
+});
+
+// NOTE: All actions would be dispatch to all registered reducers. Only the reducer concerned 
+// with a particular action would handle it, others would simply returns the state unchanged.
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 85000 }));
+const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 250 }));
 
 const demoState = {
     expenses: [{
