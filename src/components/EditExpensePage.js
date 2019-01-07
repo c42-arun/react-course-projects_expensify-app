e6d25ae64081df4487d940274cb83326fa697f0d
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2019-01-07 07:23:31 +0000

    Route params

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
-const EditExpensePage = () => (
-    <div>This is the expensify edit component</div>
-);
+const EditExpensePage = (props) => {
+    console.log(props);
+    return (
+        <div>Now editing expense with id {props.match.params.id}</div>
+    );
+};
 
 export default EditExpensePage;
