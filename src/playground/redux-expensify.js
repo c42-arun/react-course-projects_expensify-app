ca01782754ecfa7fb802502625bb07366aea1603
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2019-01-07 14:21:09 +0000

    State structure built using multiple reducers with combineReducers()

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,5 +1,6 @@
 import { createStore, combineReducers } from 'redux';
 
+// Expenses Reducer
 const expensesReducerDefaultState = [];
 
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
@@ -9,9 +10,31 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
     }
 };
 
-const store = createStore(expensesReducer);
+// Filters Reducer
+const filtersReducerDefaultState = {
+    text: '',
+    sortyBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+};
 
-console.log(store.getState()); // would print state as simply '[]', not as { expenses: [] }
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+// Store creation
+// The object passed to combineReducers defines how the state would look like
+const store = createStore(
+    combineReducers({
+        expenses: expensesReducer,
+        filters: filtersReducer
+    })
+);
+
+console.log(store.getState()); // now would print state as { expenses: [], filters: { <filtersReducerDefaultState>} }
 
 const demoState = {
     expenses: [{
