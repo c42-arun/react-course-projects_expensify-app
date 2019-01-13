f2ac8e37e24db28ff4100dbd24c610d24f4a915c
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2019-01-13 12:59:40 +0000

    Using dispatch/state in select dropdown

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { setTextFilter } from '../actions/filters'; // action generator for text filter action
+import { setTextFilter, sortByAmount, sortByDate } from '../actions/filters'; // action generator for text filter action}
 
 // a component that reads as well as sets state
 const ExpensesListFilters = (props) => (
     <div>
         <input type='text'
-            value={props.filters.text}    
+            value={props.filters.text} // <-- this is referred as "controlled input" meaning the input is controlled using Javascript
             // props also gets added with store's 'dispatch' method
-            onChange={(e) => { props.dispatch(setTextFilter(e.target.value)) }} />
+            onChange={(e) => { props.dispatch(setTextFilter(e.target.value)) }}
+        />
+        <select
+            value={props.filters.sortBy}
+            onChange={(e) => {
+                if (e.target.value === "amount") {
+                    props.dispatch(sortByAmount());
+                } else if (e.target.value === "date") {
+                    process.dispatch(sortByDate());
+                }
+            }}
+        >
+            <option value="amount">Amount</option>
+            <option value="date">Date</option>
+        </select>
     </div>
 );
 
