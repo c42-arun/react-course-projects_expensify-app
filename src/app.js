160eb785e22140eecb4dde80e819ddab80ef4a04
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2019-01-13 13:00:02 +0000

    Comented off redux demo code in app.js

--- src/app.js
+++ src/app.js
@@ -18,33 +18,34 @@ const store = configureStore();
 // actions - add expenses to state
 store.dispatch(addExpense({ description: 'Water bill', amount: 2700, createdAt: 720000 }));
 store.dispatch(addExpense({ description: 'Gas bill', amount: 5400, createdAt: 950000 }));
+store.dispatch(addExpense({ description: 'Leccy bill', amount: 2000, createdAt: 10000000 }));
 
-// adds another expense after 3 seconds - so it shows in ExpenseList automatically (just the count for now)
-setTimeout(() => {
-    store.dispatch(addExpense({ description: 'Leccy bill', amount: 2000, createdAt: 10000000 }));
-}, 3000);
+// // adds another expense after 3 seconds - so it shows in ExpenseList automatically (just the count for now)
+// setTimeout(() => {
+//     store.dispatch(addExpense({ description: 'Leccy bill', amount: 2000, createdAt: 10000000 }));
+// }, 3000);
 
-// actions - add view filters to state
-store.dispatch(setStartDate(730000));
-let state = store.getState();
+// // actions - add view filters to state
+// store.dispatch(setStartDate(730000));
+// let state = store.getState();
 
-// selector - filter state expenses based on state filters
-let visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
+// // selector - filter state expenses based on state filters
+// let visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+// console.log(visibleExpenses);
 
-store.dispatch(setStartDate());
+// store.dispatch(setStartDate());
 
-store.dispatch(setTextFilter('water'));
-state = store.getState();
+// store.dispatch(setTextFilter('water'));
+// state = store.getState();
 
-setTimeout(() => {
-    store.dispatch(setTextFilter());
-}, 6000);
+// setTimeout(() => {
+//     store.dispatch(setTextFilter());
+// }, 6000);
 
 
-// test selector
-visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
+// // test selector
+// visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+// console.log(visibleExpenses);
 
 /** REDUX DEMO END */ 
 
