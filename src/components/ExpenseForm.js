39add1e76e19945d8e5eb98255483100ad66fa9a
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2019-01-14 10:34:06 +0000

    'react-dates' Date UI component

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import moment from 'moment'; // SingleDatePicker API works with moment dates
+import { SingleDatePicker } from 'react-dates'; // https://github.com/airbnb/react-dates
+import 'react-dates/lib/css/_datepicker.css'; // CSS for SingleDatePicker
 
 export default class ExpenseForm extends React.Component {
     state = {
         description: '',
-        note: ''
+        note: '',
+        amount:'',
+        createdAt: moment(), // initialize with moment object representing today's date (as reqd by SingleDatePicker below)
+        calendarFocused: false, // required by SingleDatePicker to show/hide the calendar - hidden to start with
     };
 
     onDescriptionChange = (e) => {
@@ -27,6 +33,8 @@ export default class ExpenseForm extends React.Component {
         }
     };
 
+    onDateChange = createdAt => this.setState({ createdAt });
+
     render() {
         return (
             <div>
@@ -43,6 +51,15 @@ export default class ExpenseForm extends React.Component {
                         placeholder="Amount"
                         value={this.state.amount}
                         onChange={this.onAmountChange}
+                    />
+                    <SingleDatePicker
+                        date={this.state.createdAt}
+                        onDateChange={this.onDateChange}
+                        focused={this.state.calendarFocused}
+                        onFocusChange={({ focused }) => this.setState({ calendarFocused: focused })} // PropTypes.func.isRequired
+                        id="your_unique_id"
+                        numberOfMonths={1}
+                        isOutsideRange={day => (false)}
                     />
                     <textarea
                         placeholder="Enter an expense note (optional)"
