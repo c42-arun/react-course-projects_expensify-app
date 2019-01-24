7ae5bc1146bbcf82ceac18235696730056cc1a4f
Arunkumar Ramachandriya <arun@arunkumars-mbp.home>
2019-01-24 17:14:04 +0000

    Couple more sample test cases

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -1,7 +1,21 @@
 const add = (a, b) => a + b;
 
+const generateGreeting = (name = 'Anonymous') => `Hello ${name}!`;
+
 test('should add two numbers', () => {
     const result = add(4, 5);
 
     expect(result).toBe(9);
+});
+
+test('should generate greeting with name', () => {
+    const result = generateGreeting('Arun');
+
+    expect(result).toBe('Hello Arun!');
+});
+
+test('should generate greeting with no name', () => {
+    const result = generateGreeting();
+
+    expect(result).toBe('Hello Anonymous!');
 });
